cca215876fcc8b70a05e5536cad5e38e963f0e9f
David Mims <dmims@users.noreply.github.com>
2020-07-16 13:05:14 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html>
+  <link rel="stylesheet" href="style.css">
   <div class="header">
     <h1>Header</h1>
     <p>My supercool header</p>
@@ -16,7 +17,7 @@
   <div class="side">...</div>
   <div class="main">...</div>
   </div>
-  
+
   <div class="footer">
   <h2>Footer</h2>
   </div>
