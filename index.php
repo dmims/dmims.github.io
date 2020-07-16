159ea6384266db1ce31de6c012b5317946bad3c3
David Mims <dmims@users.noreply.github.com>
2020-07-16 13:03:53 -0400

    new

--- index.php
+++ index.php
@@ -1,9 +1,23 @@
 <!doctype html>
 <html>
-  <header>
-      <center>Testing</center>
-  </header>
-  <body>
-      
-  </body>
+  <div class="header">
+    <h1>Header</h1>
+    <p>My supercool header</p>
+  </div>
+
+  <div class="navbar">
+    <a href="#">Link</a>
+    <a href="#">Link</a>
+    <a href="#">Link</a>
+    <a href="#" class="right">Link</a>
+  </div>
+
+  <div class="row">
+  <div class="side">...</div>
+  <div class="main">...</div>
+  </div>
+  
+  <div class="footer">
+  <h2>Footer</h2>
+  </div>
 </html>
